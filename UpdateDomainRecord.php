d76d4dedeb002c08bfa0df12a01aa38e1b9878a3
YilinSun <16216077@qq.com>
2016-06-08 14:46:57 +0800

    Signed-off-by: YilinSun <16216077@qq.com>

--- UpdateDomainRecord.php
+++ UpdateDomainRecord.php
@@ -1,61 +1,50 @@
 <?php
+
 require_once('aliyunurl.php');
 
 
-$a['Action'] = 'DescribeDomainRecords';
-$a['DomainName'] = 'test.ejshendeng.com';
+$c_url = new aliyunurl();
 
 
-
-/* 
- * 修改阿里云解析记录
- */
-
-function aliyurl($a) {
-
-    $b = $a;
-
-    $b['AccessKeyId'] = '1VeNBNaqVPp7lzFs';
-    $b['Format'] = 'JSON';
-    $b['SignatureMethod'] = 'HMAC-SHA1';
-    $b['SignatureNonce'] = time();
-    $b['SignatureVersion'] = '1.0';
-
-    //$b['Timestamp'] = '2016-03-24T16:41:54Z';
-    $b['Timestamp'] = date('Y-m-d', time()) . 'T' . date('H:i:s', time()) . 'Z';
-
-    $b['Version'] = '2015-01-09';
-
-
-    ksort($b);
-
-    $s = 'GET';
-    $s .= '&';
-    $s .= rawurlencode('/');
-    $s .= '&';
-
-    $c = array();
-
-    foreach ($b as $k => $v) {
-        if ('Timestamp' == $k) {
-            $v = rawurlencode($v);
-        }
-        $c[] = ($k . '=' . $v);
-    }
-
-    $str = join('&', $c);
-
-    $StringToSign = $s . rawurlencode($str);
-
-    /*Rw3OQGVOVl7w8NS5vdUErRCcTnIIoi是在阿里云上的Access Key Secret*/
-    $Signature = base64_encode(hash_hmac("sha1", $StringToSign, 'Rw3OQGVOVl7w8NS5vdUErRCcTnIIoi&', true));
-
-
-
-    $url = 'http://alidns.aliyuncs.com/?';
-    $url .= $str;
-    $url .= '&Signature=' . $Signature;
-
-    return $url;
+if (isset($_GET['act'])) {
+    $act = $_GET['act'];
+} else {
+    $act = '';
 }
 
+
+switch ($act) {
+    case '':
+        htmlmain();
+        break;
+    case 'set':
+        SetRecordStatus();
+        break;
+}
+
+function htmlmain() {
+    $c_url = & $GLOBALS['c_url'];
+
+    $a['Action'] = 'DescribeDomainRecords';
+    $a['DomainName'] = 'www.ejshendeng.com';
+
+    $url = $c_url->makeurl($a);
+
+    $result = $c_url->getcongent($url);
+
+    print_r($result);
+}
+
+function SetRecordStatus() {
+    $c_url = & $GLOBALS['c_url'];
+
+    $a['Action'] = 'SetDomainRecordStatus';
+    $a['RecordId'] = '75868105';
+    $a['Status'] = 'Enable';
+    
+    $url = $c_url->makeurl($a);
+
+    $result = $c_url->getcongent($url);
+
+    print_r($result);
+}
